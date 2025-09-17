ca2e5de74e3f2cd7e163127cc749b293ab99584d
AdyG (2-60) <adrian.gould@nmtafe.wa.edu.au>
2025-09-17 16:47:32 +0800

    wip(chirp): Part 2 - Displaying Chirps
    Add:
    - Chirp display on chirps index
    - Chirp navigation link

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -16,6 +16,42 @@
 
         </form>
 
+
+        <div class="mt-6 bg-white shadow-sm
+			rounded-lg divide-y">
+
+            @foreach ($chirps as $chirp)
+
+                <div class="p-6 flex space-x-2">
+
+                    <i
+                        class="fa-regular fa-comment-dots
+	              fa-shake
+	              text-xl text-blue-400"
+                        style="--fa-animation-duration: 2s;
+                   --fa-animation-iteration-count: 2;
+                  --fa-animation-timing: ease-in-out;"
+                        aria-hidden="true"></i>
+                    <div class="flex-1">
+                        <div class="flex justify-between items-center">
+                            <div>
+                <span class="text-gray-800">
+                {{ $chirp->user->name }}
+                </span>
+                                <small class="ml-2
+			                 text-sm text-gray-600">
+                                    {{ $chirp->created_at->format('j M Y, g:i a') }}
+                                </small>
+                            </div>
+                        </div>
+                        <p class="mt-4 text-lg text-gray-900">
+                            {{ $chirp->message }}
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
     </div>
 
 </x-app-layout>
